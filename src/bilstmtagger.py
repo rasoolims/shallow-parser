940398f60c927ff191555bd956e2a8bd52447f93
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2016-11-07 15:30:06 -0500

    added batch train

--- src/bilstmtagger.py
+++ src/bilstmtagger.py
@@ -69,7 +69,6 @@ class Tagger:
         if sent: yield  sent
 
     def build_tagging_graph(self, words, tags, bios):
-        renew_cg()
         f_init, b_init = [b.initial_state() for b in self.builders]
         wembs = [noise(self.WE[w], 0.1) for w in words]
         pembs = [noise(self.PE[t],0.001) for t in tags]
@@ -125,6 +124,7 @@ class Tagger:
     def train(self):
         tagged = loss = 0
         best_dev = float('-inf')
+        errs = []
         for ITER in xrange(self.options.epochs):
             print 'ITER', ITER
             random.shuffle(train)
@@ -157,12 +157,25 @@ class Tagger:
                 ps = [self.vt.w2i[p] for w, p, bio in s]
                 bs = [self.vb.w2i[bio] for w, p, bio in s]
                 sum_errs = self.build_tagging_graph(ws, ps, bs)
-                squared = -sum_errs  # * sum_errs
-                loss += sum_errs.scalar_value()
+                #squared = -sum_errs  # * sum_errs
+                #loss += sum_errs.scalar_value()
+                errs.append(sum_errs)
                 tagged += len(ps)
-                sum_errs.backward()
-                self.trainer.update()
+                if len(errs)>50:
+                    eerrs = esum(errs)
+                    scalar_loss = eerrs.scalar_value()
+                    eerrs.backward()
+                    self.trainer.update()
+                    errs = []
+                    renew_cg()
             print 'saving current iteration'
+            if len(errs) > 0:
+                eerrs = esum(errs)
+                scalar_loss = eerrs.scalar_value()
+                eerrs.backward()
+                self.trainer.update()
+                errs = []
+                renew_cg()
             self.save(os.path.join(options.output, options.model + '_' + str(ITER)))
 
     def load(self, f):
@@ -191,7 +204,6 @@ class Tagger:
         parser.add_option("--eval", action="store_true", dest="eval_format", default=False)
         return parser.parse_args()
 
-#todo add external embeddings
 if __name__ == '__main__':
     (options, args) = Tagger.parse_options()
     if options.conll_train != '' and options.output != '':
