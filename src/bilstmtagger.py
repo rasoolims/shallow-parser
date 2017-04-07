56a1b9e58b8c82286a62d6ea9057d71951dddda0
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2017-04-07 16:21:38 -0400

    inheritance

--- src/bilstmtagger.py
+++ src/bilstmtagger.py
@@ -1,118 +1,22 @@
-from collections import Counter
-import random,os,codecs,pickle,time
-from optparse import OptionParser
-import numpy as np
+from postagger import  *
 
-import util
-
-def parse_options():
-    parser = OptionParser()
-    parser.add_option('--train', dest='conll_train', help='Annotated CONLL train file', metavar='FILE', default='')
-    parser.add_option('--dev', dest='dev_file', help='Annotated CONLL development file', metavar='FILE', default=None)
-    parser.add_option('--test', dest='conll_test', help='Annotated CONLL test file', metavar='FILE', default='')
-    parser.add_option('--inputs', dest='inputs', help='Input tagged files separated by ,', metavar='FILE', default=None)
-    parser.add_option('--ext', dest='ext', help='File extension for outputfiles', type='str', default='.chunk')
-    parser.add_option('--params', dest='params', help='Parameters file', metavar='FILE', default='params.pickle')
-    parser.add_option('--extrn', dest='external_embedding', help='External embeddings', metavar='FILE')
-    parser.add_option('--init', dest='initial_embeddings', help='Initial embeddings', metavar='FILE')
-    parser.add_option('--model', dest='model', help='Load/Save model file', metavar='FILE', default='model.model')
-    parser.add_option('--wembedding', type='int', dest='wembedding_dims', default=128)
-    parser.add_option('--cembedding', type='int', dest='cembedding_dims', help='size of character embeddings',
-                      default=30)
-    parser.add_option('--pembedding', type='int', dest='pembedding_dims', default=30)
-    parser.add_option('--epochs', type='int', dest='epochs', default=5)
-    parser.add_option('--pos_epochs', type='int', dest='pos_epochs', default=2)
-    parser.add_option('--hidden', type='int', dest='hidden_units', default=200)
-    parser.add_option('--hidden2', type='int', dest='hidden2_units', default=0)
-    parser.add_option('--lstmdims', type='int', dest='lstm_dims', default=200)
-    parser.add_option('--tlstmdims', type='int', dest='tag_lstm_dims', default=200)
-    parser.add_option('--clstmdims', type='int', dest='clstm_dims', default=100)
-    parser.add_option('--outdir', type='string', dest='output', default='')
-    parser.add_option('--outfile', type='string', dest='outfile', default='')
-    parser.add_option("--eval", action="store_true", dest="eval_format", default=False)
-    parser.add_option("--activation", type="string", dest="activation", default="tanh")
-    parser.add_option("--drop", action="store_true", dest="drop", default=False, help='Use dropout.')
-    parser.add_option("--gru", action="store_true", dest="gru", default=False, help='Use GRU instead of LSTM.')
-    parser.add_option("--save_best", action="store_true", dest="save_best", default=False, help='Store the best model.')
-    parser.add_option("--dropout", type="float", dest="dropout", default=0.33, help='Dropout probability.')
-    parser.add_option('--mem', type='int', dest='mem', default=2048)
-    parser.add_option('--k', type='int', dest='k', help='word LSTM depth', default=1)
-    parser.add_option('--batch', type='int', dest='batch', default=50)
-    return parser.parse_args()
-
-import _dynet as dy
-(options, args) = parse_options()
-dyparams = dy.DynetParams()
-dyparams.from_args()
-dyparams.set_mem(options.mem)
-dyparams.init()
-from dynet import *
-
-class Chunker:
+class Chunker(Tagger):
     def __init__(self, options, words, tags, bios, chars):
-        self.options = options
+        Tagger.__init__(self, options, words, tags, chars)
         self.activations = {'tanh': tanh, 'sigmoid': logistic, 'relu': rectify}
         self.activation = self.activations[options.activation]
-        self.vw = util.Vocab.from_corpus([words])
         self.vb = util.Vocab.from_corpus([bios])
-        self.vt = util.Vocab.from_corpus([tags])
-        self.UNK_W = self.vw.w2i['_UNK_']
-        self.batch = options.batch
-        self.nwords = self.vw.size()
-        self.chars = util.Vocab.from_corpus([chars])
-        self.ntags = self.vt.size()
         self.nBios = self.vb.size()
-        print 'num of pos tags',self.ntags, 'num of bio tags',self.nBios
-        self.model = Model()
-        self.trainer = AdamTrainer(self.model)
+        print  'num of bio tags',self.nBios
 
-        self.WE = self.model.add_lookup_parameters((self.nwords, options.wembedding_dims))
-        self.CE = self.model.add_lookup_parameters((self.chars.size(), options.cembedding_dims))
-        self.pH1 = self.model.add_parameters((options.hidden_units, options.lstm_dims)) if options.hidden_units > 0 else None
-        self.pH2 = self.model.add_parameters((options.hidden2_units, options.hidden_units)) if options.hidden2_units > 0 else None
+        self.H1 = self.model.add_parameters((options.hidden_units, options.lstm_dims)) if options.hidden_units > 0 else None
+        self.H2 = self.model.add_parameters((options.hidden2_units, options.hidden_units)) if options.hidden2_units > 0 else None
         hdim = options.hidden2_units if options.hidden2_units>0 else options.hidden_units if options.hidden_units>0 else options.lstm_dims
-        self.pO = self.model.add_parameters((self.nBios, hdim))
-        self.k = options.k
-        self.drop = options.drop
-        self.dropout = options.dropout
+        self.O = self.model.add_parameters((self.nBios, hdim))
         self.transitions = self.model.add_lookup_parameters((self.nBios, self.nBios))
-        self.tag_transitions = self.model.add_lookup_parameters((self.ntags, self.ntags))
-        self.edim = 0
-        self.external_embedding = None
-        if options.initial_embeddings is not None:
-            initial_embeddings_fp = open(options.initial_embeddings, 'r')
-            initial_embeddings_fp.readline()
-            initial_embeddings_vec = {line.split(' ')[0]: [float(f) for f in line.strip().split(' ')[1:]] for line in
-                                       initial_embeddings_fp}
-            initial_embeddings_fp.close()
-            for word in self.vw.w2i.keys():
-               if word in initial_embeddings_vec:
-                   assert options.wembedding_dims == len(initial_embeddings_vec[word])
-                   self.WE.init_row(self.vw.w2i.get(word), initial_embeddings_vec[word])
-        if options.external_embedding is not None:
-            external_embedding_fp = open(options.external_embedding, 'r')
-            external_embedding_fp.readline()
-            self.external_embedding = {line.split(' ')[0]: [float(f) for f in line.strip().split(' ')[1:]] for line in
-                                       external_embedding_fp}
-            external_embedding_fp.close()
-            self.edim = len(self.external_embedding.values()[0])
-            noextrn = [0.0 for _ in xrange(self.edim)]
-            self.extrnd = {word: i + 3 for i, word in enumerate(self.external_embedding)}
-            self.extrn_lookup = self.model.add_lookup_parameters((len(self.external_embedding) + 3, self.edim))
-            self.extrn_lookup.set_updated(False)
-            for word, i in self.extrnd.iteritems():
-                self.extrn_lookup.init_row(i, self.external_embedding[word])
-            self.extrnd['_UNK_'] = 1
-            self.extrnd['_START_'] = 2
-            self.extrn_lookup.init_row(1, noextrn)
-            print 'Loaded external embedding. Vector dimensions:', self.edim
 
-        tag_inp_dim = options.wembedding_dims + self.edim + options.clstm_dims
-        inp_dim = tag_inp_dim + self.ntags
+        inp_dim = options.wembedding_dims + self.edim + options.clstm_dims + self.ntags
         self.chunk_lstms = BiRNNBuilder(self.k, inp_dim, options.lstm_dims, self.model, LSTMBuilder if not options.gru else GRUBuilder)
-        self.tag_lstms = BiRNNBuilder(self.k, tag_inp_dim, options.tag_lstm_dims, self.model, LSTMBuilder if not options.gru else GRUBuilder)
-        self.char_lstms = BiRNNBuilder(1, options.cembedding_dims, options.clstm_dims, self.model, LSTMBuilder if not options.gru else GRUBuilder)
-        self.tagO = self.model.add_parameters((self.ntags, options.tag_lstm_dims))
 
     @staticmethod
     def read(fname):
@@ -126,17 +30,6 @@ class Chunker:
                 w, p, bio = line
                 sent.append((w, p, bio))
         if sent: yield  sent
-
-    @staticmethod
-    def read_tagged_file(fname):
-        for line in file(fname):
-            spl = line.strip().split()
-            sent = []
-            for s in spl:
-                w = s[:s.rfind('_')]
-                p = s[s.rfind('_')+1:]
-                sent.append((w,p,'_'))
-            yield sent
 
     def build_pos_graph(self, sent_words, words, is_train):
         input_lstm = self.get_lstm_features(is_train, sent_words, words, False)[0]
@@ -182,96 +75,21 @@ class Chunker:
 
     def build_tagging_graph(self, sent_words, words, is_train):
         input_lstm,pos_probs = self.get_lstm_features(is_train, sent_words, words, True)
-        H1 = parameter(self.pH1) if self.pH1!=None else None
-        H2 = parameter(self.pH2) if self.pH2!=None else None
-        O = parameter(self.pO)
+        H1 = parameter(self.H1) if self.H1 != None else None
+        H2 = parameter(self.H2) if self.H2 != None else None
+        O = parameter(self.O)
         scores = []
 
         for f in input_lstm:
-            score_t = O*(self.activation(H2*self.activation(H1 * f))) if H2!=None else O * (self.activation(H1 * f)) if self.pH1!=None  else O*f
+            score_t = O*(self.activation(H2*self.activation(H1 * f))) if H2!=None else O * (self.activation(H1 * f)) if self.H1 != None  else O * f
             scores.append(score_t)
         return scores,pos_probs
-
-    def score_sentence(self, observations, labels, trans_matrix, dct):
-        assert len(observations) == len(labels)
-        score_seq = [0]
-        score = scalarInput(0)
-        labels = [dct['_START_']] + labels
-        for i, obs in enumerate(observations):
-            score = score + pick(trans_matrix[labels[i+1]],labels[i]) + pick(obs, labels[i+1])
-            score_seq.append(score.value())
-        score = score + pick(trans_matrix[dct['_STOP_']],labels[-1])
-        return score
-
-    def forward(self, observations, ntags, trans_matrix, dct):
-        def log_sum_exp(scores):
-            npval = scores.npvalue()
-            argmax_score = np.argmax(npval)
-            max_score_expr = pick(scores, argmax_score)
-            max_score_expr_broadcast = concatenate([max_score_expr] * ntags)
-            return max_score_expr + log(sum_cols(transpose(exp(scores - max_score_expr_broadcast))))
-
-        init_alphas = [-1e10] * ntags
-        init_alphas[dct['_START_']] = 0
-        for_expr = inputVector(init_alphas)
-        for obs in observations:
-            alphas_t = []
-            for next_tag in range(ntags):
-                obs_broadcast = concatenate([pick(obs, next_tag)] * ntags)
-                next_tag_expr = for_expr + trans_matrix[next_tag] + obs_broadcast
-                alphas_t.append(log_sum_exp(next_tag_expr))
-            for_expr = concatenate(alphas_t)
-        terminal_expr = for_expr + trans_matrix[dct['_STOP_']]
-        alpha = log_sum_exp(terminal_expr)
-        return alpha
-
-    def viterbi_decoding(self, observations, trans_matrix, dct, nL):
-        backpointers = []
-        init_vvars   = [-1e10] * nL
-        init_vvars[dct['_START_']] = 0 # <Start> has all the probability
-        for_expr = inputVector(init_vvars)
-        trans_exprs  = [trans_matrix[idx] for idx in range(nL)]
-        for obs in observations:
-            bptrs_t = []
-            vvars_t = []
-            for next_tag in range(nL):
-                next_tag_expr = for_expr + trans_exprs[next_tag]
-                next_tag_arr = next_tag_expr.npvalue()
-                best_tag_id  = np.argmax(next_tag_arr)
-                bptrs_t.append(best_tag_id)
-                vvars_t.append(pick(next_tag_expr, best_tag_id))
-            for_expr = concatenate(vvars_t) + obs
-            backpointers.append(bptrs_t)
-        # Perform final transition to terminal
-        terminal_expr = for_expr + trans_exprs[dct['_STOP_']]
-        terminal_arr  = terminal_expr.npvalue()
-        best_tag_id = np.argmax(terminal_arr)
-        path_score  = pick(terminal_expr, best_tag_id)
-        # Reverse over the backpointers to get the best path
-        best_path = [best_tag_id] # Start with the tag that was best for terminal
-        for bptrs_t in reversed(backpointers):
-            best_tag_id = bptrs_t[best_tag_id]
-            best_path.append(best_tag_id)
-        start = best_path.pop() # Remove the start symbol
-        best_path.reverse()
-        assert start == dct['_START_']
-        # Return best path and best path's score
-        return best_path, path_score
 
     def neg_log_loss(self, sent_words, words, labels, is_chunking):
         observations = self.build_tagging_graph(sent_words, words, True)[0] if is_chunking else self.build_pos_graph(sent_words, words, True)
         gold_score = self.score_sentence(observations, labels, self.transitions if is_chunking else self.tag_transitions, self.vb.w2i if is_chunking else self.vt.w2i)
         forward_score = self.forward(observations, self.nBios if is_chunking else self.ntags, self.transitions if is_chunking else self.tag_transitions, self.vb.w2i if is_chunking else self.vt.w2i)
         return forward_score - gold_score
-
-    def viterbi_loss(self,  sent_words,words,tags, bios):
-        observations = self.build_tagging_graph(sent_words,words,tags, True)[0]
-        viterbi_tags, viterbi_score = self.viterbi_decoding(observations,self.transitions,self.vb.w2i, self.nBios)
-        if viterbi_tags != bios:
-            gold_score = self.score_sentence(observations, bios, self.transitions, self.vb.w2i)
-            return (viterbi_score - gold_score), viterbi_tags
-        else:
-            return dy.scalarInput(0), viterbi_tags
 
     def tag_sent(self, sent):
         renew_cg()
@@ -359,11 +177,6 @@ class Chunker:
                 print 'dev pos accuracy', pos_res
         return best_dev
 
-    def load(self, f):
-        self.model.load(f)
-
-    def save(self, f):
-        self.model.save(f)
 
 if __name__ == '__main__':
     if options.conll_train != '' and options.output != '':
@@ -421,31 +234,4 @@ if __name__ == '__main__':
                 [output.append(' '.join([sent[i][0], pos_tags[i], bio_tags[i]])) for i in xrange(len(bio_tags))]
             writer.write('\n'.join(output))
             writer.write('\n\n')
-        print 'done!'
-
-    if options.inputs != None and options.params != '' and options.model != '':
-        print options.model, options.params, options.eval_format
-        print 'reading params'
-        with open(options.params, 'r') as paramsfp:
-            words, bio_tags, bios, ch, opt = pickle.load(paramsfp)
-        tagger = Chunker(opt, words, bio_tags, bios, ch)
-        print 'loading model'
-        print options.model
-        tagger.load(options.model)
-
-        inputs = options.inputs.strip().split(',')
-        for input in inputs:
-            print input
-            test = list(Chunker.read_tagged_file(input))
-            print 'loaded',len(test),'sentences!'
-            writer = codecs.open(input+options.ext, 'w')
-            for sent in test:
-                output = list()
-                bio_tags,pos_tags = tagger.tag_sent(sent)
-                if options.eval_format:
-                     [output.append(' '.join([sent[i][0], pos_tags[i][1], sent[i][2], bio_tags[i]])) for i in xrange(len(bio_tags))]
-                else:
-                    [output.append(' '.join([sent[i][0], pos_tags[i][1], bio_tags[i]])) for i in xrange(len(bio_tags))]
-                writer.write('\n'.join(output))
-                writer.write('\n\n')
         print 'done!'
