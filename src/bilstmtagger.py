a784c4fe0eca64d563fbdcc31d8219c9f2be8661
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2017-04-25 11:26:29 -0400

    ...

--- src/bilstmtagger.py
+++ src/bilstmtagger.py
@@ -51,7 +51,7 @@ class Chunker(Tagger):
         for_expr = inputVector(init_alphas)
 
         for i in xrange(len(observations)):
-            fes = []
+            fes = []*(len(observations)-i)
             for k in xrange(len(observations)-i):
                 feat = observations[k] - observations[i-1] if i>=0 else observations[k]
                 alphas_t = []
@@ -59,7 +59,7 @@ class Chunker(Tagger):
                     obs_broadcast = concatenate([pick(feat, next_tag)] * ntags)
                     next_tag_expr = for_expr + trans_matrix[next_tag] + obs_broadcast
                     alphas_t.append(log_sum_exp(next_tag_expr))
-                    fes.append(concatenate(alphas_t))
+                    fes[i].append(concatenate(alphas_t))
             for_expr = sum_cols(concatenate_cols(fes))
         terminal_expr = for_expr + trans_matrix[dct['_STOP_']]
         alpha = log_sum_exp(terminal_expr)
