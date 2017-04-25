834e96d83e461b055d0039fe6ac8f97ba580c741
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2017-04-25 11:10:48 -0400

    first draft (not-tested) of semi-crf

--- src/bilstmtagger.py
+++ src/bilstmtagger.py
@@ -9,6 +9,7 @@ class Chunker(Tagger):
         self.activation = self.activations[options.activation]
         self.vl = util.Vocab.from_corpus([labels]) # no-bio tags
         self.nLabels = self.vl.size()
+        print  'num of labels',self.nLabels
         self.PE = self.model.add_lookup_parameters((self.ntags, options.pembedding_dims))
         self.H1 = self.model.add_parameters((options.hidden_units, options.lstm_dims)) if options.hidden_units > 0 else None
         self.H2 = self.model.add_parameters((options.hidden2_units, options.hidden_units)) if options.hidden2_units > 0 else None
@@ -23,7 +24,6 @@ class Chunker(Tagger):
             self.WE.init_row(i, self.pos_tagger.WE[i].npvalue())
         for i in xrange(self.chars.size()):
             self.CE.init_row(i, self.pos_tagger.CE[i].npvalue())
-
 
     @staticmethod
     def read(fname):
@@ -77,38 +77,73 @@ class Chunker(Tagger):
         input_lstm = self.chunk_lstms.transduce(inputs)
         return input_lstm
 
+    def score_sentence_semi(self, observations, segments, trans_matrix, dct):
+        score_seq = [0]
+        score = scalarInput(0)
+        labels = [dct['_START_']] + [dct[l] for _,_,l in segments]
+
+        for i in xrange(len(segments)):
+            s,e,_ = segments[i]
+            score = score + pick(trans_matrix[labels[i+1]],labels[i]) + pick(observations[e], labels[i+1]) - (pick(observations[s-1], labels[i]) if s>0 else scalarInput(0))
+            score_seq.append(score.value())
+        score = score + pick(trans_matrix[dct['_STOP_']],labels[-1])
+        return score
+
     def viterbi_decoding_semi(self, observations, trans_matrix, dct, nL):
         backpointers = []
         init_vvars   = [-1e10] * nL
         init_vvars[dct['_START_']] = 0 # <Start> has all the probability
-        for_expr = inputVector(init_vvars)
+        for_expr = [inputVector(0)]*(len(observations)+1)
+        for_expr[0] = inputVector(init_vvars)
         trans_exprs  = [trans_matrix[idx] for idx in range(nL)]
-        for obs in observations:
+        for i in xrange(len(observations)):
             bptrs_t = []
             vvars_t = []
             for next_tag in range(nL):
-                next_tag_expr = for_expr + trans_exprs[next_tag]
-                next_tag_arr = next_tag_expr.npvalue()
-                best_tag_id  = np.argmax(next_tag_arr)
-                bptrs_t.append(best_tag_id)
-                vvars_t.append(pick(next_tag_expr, best_tag_id))
-            for_expr = concatenate(vvars_t) + obs
+                max_pointer = 0
+                max_value = float('-inf')
+                for k in xrange(i+1):
+                    next_tag_expr = for_expr[k] + trans_exprs[next_tag] + observations[i] - (observations[k-1] if k>0 else inputVector(0))
+                    next_tag_arr = next_tag_expr.npvalue()
+                    best_tag_id  = np.argmax(next_tag_arr)
+                    v = pick(next_tag_expr, best_tag_id).value()
+                    if v>max_value:
+                        max_value = v
+                        max_pointer =best_tag_id,k,pick(next_tag_expr, best_tag_id)
+                bptrs_t.append((max_pointer[0],max_pointer[1]))
+                vvars_t.append(max_pointer[2])
+            for_expr[i] = concatenate(vvars_t)
             backpointers.append(bptrs_t)
         # Perform final transition to terminal
-        terminal_expr = for_expr + trans_exprs[dct['_STOP_']]
+        terminal_expr = for_expr[-1] + trans_exprs[dct['_STOP_']]
         terminal_arr  = terminal_expr.npvalue()
         best_tag_id = np.argmax(terminal_arr)
         path_score  = pick(terminal_expr, best_tag_id)
         # Reverse over the backpointers to get the best path
-        best_path = [best_tag_id] # Start with the tag that was best for terminal
+        segments = []
+        end = len(observations)-1
         for bptrs_t in reversed(backpointers):
-            best_tag_id = bptrs_t[best_tag_id]
-            best_path.append(best_tag_id)
-        start = best_path.pop() # Remove the start symbol
-        best_path.reverse()
+            best_tag_id,start = bptrs_t[best_tag_id]
+            segments.append((start,end,best_tag_id))
+            end = start-1
+        #start = segments.pop() # Remove the start symbol
+        segments.reverse()
+
+        bios = []
+        for s,e,l in segments:
+            if l == 'O':
+                bios.append(l)
+            else:
+                bios.append('B-'+l)
+            for i in range(s,e):
+                if l == 'O':
+                    bios.append(l)
+                else:
+                    bios.append('I-' + l)
+
         assert start == dct['_START_']
         # Return best path and best path's score
-        return best_path, path_score
+        return bios, path_score
 
     def build_graph(self, sent_words, words, auto_tags, is_train):
         input_lstm = self.get_chunk_lstm_features(is_train, sent_words, words, auto_tags)
@@ -122,9 +157,9 @@ class Chunker(Tagger):
             scores.append(score_t)
         return scores
 
-    def neg_log_loss(self, sent_words, words, labels, auto_tags):
+    def neg_log_loss(self, sent_words, words, segments, auto_tags):
         observations = self.build_graph(sent_words, words, auto_tags, True)
-        gold_score = self.score_sentence(observations, labels, self.transitions, self.vb.w2i)
+        gold_score = self.score_sentence_semi(observations, segments, self.transitions, self.vl.w2i)
         forward_score = self.forward_semi(observations, self.nLabels, self.transitions, self.vl.w2i)
         return forward_score - gold_score
 
@@ -135,12 +170,11 @@ class Chunker(Tagger):
         auto_tags = self.pos_tagger.best_pos_tags(words)
         observations = self.build_graph(words, ws, auto_tags, False)
         bios, score = self.viterbi_decoding_semi(observations,self.transitions,self.vl.w2i, self.nLabels)
-        return [self.vb.i2w[b] for b in bios],[self.pos_tagger.vt.i2w[p] for p in auto_tags]
+        return bios,[self.pos_tagger.vt.i2w[p] for p in auto_tags]
 
     def train(self):
         tagged, loss = 0,0
         best_dev = float('-inf')
-        batch = 0
         for ITER in xrange(self.options.epochs):
             print 'ITER', ITER
             random.shuffle(train)
@@ -155,19 +189,19 @@ class Chunker(Tagger):
                 ws = [self.vw.w2i.get(w, self.UNK_W) for w, p, bio in s]
                 ps = [self.vt.w2i[t] for w, t, bio in s]
                 auto_tags = self.pos_tagger.best_pos_tags([w for w, p, bio in s])
-                bs = [self.vb.w2i[bio] for w, p, bio in s]
-                batch.append(([w for w,_,_ in s],ws,ps,bs,auto_tags))
-                tagged += len(ps)
 
+                segments = self.get_segments(s)
+                batch.append(([w for w,_,_ in s],ws,ps,segments,auto_tags))
+                tagged += len(ps)
 
                 if len(batch)>=self.batch:
                     errs = []
                     for j in xrange(len(batch)):
-                        sent_words,ws,ps,bs,at = batch[j]
+                        sent_words,ws,ps,segments,at = batch[j]
                         if ITER < self.options.pos_epochs:
                             errs.append(self.pos_neg_log_loss(sent_words, ws,  ps))
                         else:
-                            errs.append(self.neg_log_loss(sent_words, ws,  bs, at))
+                            errs.append(self.neg_log_loss(sent_words, ws,  segments, at))
                     sum_errs = esum(errs)
                     loss += sum_errs.scalar_value()
                     sum_errs.backward()
@@ -180,6 +214,26 @@ class Chunker(Tagger):
         if not options.save_best or not options.dev_file:
             print 'Saving the final model'
             self.save(os.path.join(options.output, options.model))
+
+    def get_segments(self, sen):
+        bios = [bio for _, _, bio in sen]
+        segments = []
+        s, e, l = 0, 0, ''
+        r = False
+        for i in xrange(len(bios)):
+            if bios[i] == 'O':
+                if r: segments.append((s, e, bios[i - 1][bios[i - 1].find('-') + 1:]))
+                segments.append((i, i, bios[i]))
+                r = False
+                s,e = i + 1,i + 1
+            elif bios[i].startswith('B-') and not r:
+                segments.append((s, e, bios[i - 1][bios[i - 1].find('-') + 1:]))
+                s,e = i,i
+            else:
+                e = i
+                r = True
+        if r: segments.append((s, e, bios[-1][bios[-1].find('-') + 1:]))
+        return segments
 
     def validate(self, best_dev):
         dev = list(self.read(options.dev_file))
@@ -224,28 +278,28 @@ if __name__ == '__main__':
         train = list(Chunker.read(options.conll_train))
         print 'load #sent:',len(train)
         words = []
-        bio_tags = []
+        tags = []
         labels = []
         chars = {' ','<s>','</s>'}
         wc = Counter()
         for s in train:
             for w, p, bio in s:
                 words.append(w)
-                bio_tags.append(p)
+                tags.append(p)
                 labels.append(bio[bio.find('-')+1:] if '-' in bio else bio)
                 [chars.add(x) for x in list(w)]
                 wc[w] += 1
         words.append('_UNK_')
         labels.append('_START_')
         labels.append('_STOP_')
-        bio_tags.append('_START_')
-        bio_tags.append('_STOP_')
+        tags.append('_START_')
+        tags.append('_STOP_')
         ch = list(chars)
 
         with open(os.path.join(options.output, options.params), 'w') as paramsfp:
-            pickle.dump((words, bio_tags, labels, ch, options), paramsfp)
+            pickle.dump((words, tags, labels, ch, options), paramsfp)
 
-        Chunker(options, words, bio_tags, labels, ch,tagger).train()
+        Chunker(options, words, tags, labels, ch, tagger).train()
 
         options.model = os.path.join(options.output,options.model)
         options.params = os.path.join(options.output,options.params)
@@ -254,8 +308,8 @@ if __name__ == '__main__':
         print options.model, options.params, options.eval_format
         print 'reading params'
         with open(options.params, 'r') as paramsfp:
-            words, bio_tags, labels, ch, opt = pickle.load(paramsfp)
-        chunker = Chunker(opt, words, bio_tags, labels, ch,tagger)
+            words, tags, labels, ch, opt = pickle.load(paramsfp)
+        chunker = Chunker(opt, words, tags, labels, ch, tagger)
 
         print 'loading model'
         print options.model
@@ -266,11 +320,11 @@ if __name__ == '__main__':
         writer = codecs.open(options.outfile, 'w')
         for sent in test:
             output = list()
-            bio_tags,pos_tags = chunker.tag_sent(sent)
+            tags, pos_tags = chunker.tag_sent(sent)
             if options.eval_format:
-                 [output.append(' '.join([sent[i][0], pos_tags[i], sent[i][2], bio_tags[i]])) for i in xrange(len(bio_tags))]
+                 [output.append(' '.join([sent[i][0], pos_tags[i], sent[i][2], tags[i]])) for i in xrange(len(tags))]
             else:
-                [output.append(' '.join([sent[i][0], pos_tags[i], bio_tags[i]])) for i in xrange(len(bio_tags))]
+                [output.append(' '.join([sent[i][0], pos_tags[i], tags[i]])) for i in xrange(len(tags))]
             writer.write('\n'.join(output))
             writer.write('\n\n')
         print 'done!'
