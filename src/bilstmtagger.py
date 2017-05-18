69a55344b78daf0a38243b3975c9d3c3f394ff7b
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2017-05-18 09:45:07 -0400

    ''  to None

--- src/bilstmtagger.py
+++ src/bilstmtagger.py
@@ -228,7 +228,7 @@ if __name__ == '__main__':
             writer.write('\n'.join(output))
             writer.write('\n\n')
         print 'done!'
-    if options.inputs != '' and options.params != '' and options.model != '':
+    if options.inputs != None and options.params != '' and options.model != '':
         print options.model, options.params, options.eval_format
         print 'reading params'
         with open(options.params, 'r') as paramsfp:
